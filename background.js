bd5b8a1335cab350e9d4a4eccbc4ce71bccad7c5
Ola Thoresen <olen@inni.no>
2025-05-15 10:28:17 +0200

    feat: implement GAL

--- background.js
+++ background.js
@@ -3,3 +3,44 @@ await browser.LegacyHelper.registerGlobalUrls([
 ]);
 
 await browser.EAS4TbSync.load();
+
+messenger.addressBooks.provider.onSearchRequest.addListener(async (node, searchString, query) => {
+    // let response = await fetch("https://people.acme.com/?query=" + searchString);
+    // let json = await response.json();
+    console.log("node:", node)
+    console.log("searchString:", searchString)
+    console.log("query:", query)
+    console.log("easbridge:", messenger.EASBridge)
+    const results = await browser.EASBridge.searchGal(searchString);
+    console.log("tbsync:", TbSync)
+    console.log("tbsync:", messenger.EAS4TbSync)
+    let gal_result = eas.network.getSearchResults(this.folderData.accountData, searchString)
+    console.log(gal_result)
+    /****
+    let json = [
+      {
+        name: "Alice",
+        email: "alice@example.com"
+      },
+      {
+        name: "Bob",
+        email: "bob@example.com"
+      },
+      {
+        name: "Carol",
+        email: "carol@example.com"
+      }
+    ];
+    return {
+        isCompleteResult: true,
+        // Return an array of ContactProperties as results.
+        results: json.map(contact => ({
+            DisplayName: contact.name,
+            PrimaryEmail: contact.email
+        }))
+    };
+    *****/
+}, {
+    addressBookName: "GAL",
+    isSecure: true,
+});
